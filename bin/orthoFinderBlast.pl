a94f24594115ebc2c7ce17bf0e63113341e9b0a2
rdemko2332 <rdemko2332@gmail.com>
2023-08-01 10:30:54 -0400

    Adding script for orthoFinder diamond job caching

--- bin/orthoFinderBlast.pl
+++ bin/orthoFinderBlast.pl
@@ -4,10 +4,38 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-my ($query,$database,$filePath,$queryNumber,$dataNumber);
+my ($query,$database,$filePath,$queryNumber,$dataNumber,$previousBlasts,$sequenceIDs,$speciesIDs);
 
 &GetOptions("query=s"=> \$query,
-            "database=s"=> \$database);
+            "database=s"=> \$database,
+	    "sequenceIDs=s"=> \$sequenceIDs,
+	    "speciesIDs=s"=> \$speciesIDs
+           );
+
+# Read species ID file
+open my $fh_species, '<', $species_id_file or die "Cannot open $species_id_file: $!";
+my %species_map;
+while (my $line = <$fh_species>) {
+    chomp $line;
+    if ($line =~ /^(\d+):\s+(\S+)\.fasta\.fasta/) {
+        my ($id, $species) = ($1, $2);
+        $species_map{$id} = $species;
+    }
+}
+close $fh_species;
+
+# Read sequences ID file
+open my $fh_sequences, '<', $sequences_id_file or die "Cannot open $sequences_id_file: $!";
+my %sequences_map;
+while (my $line = <$fh_sequences>) {
+    chomp $line;
+    if ($line =~ /^(\d+_\d+):\s+(.+)/) {
+        my ($new, $original) = ($1, $2);
+        $sequences_map{$new} = $original;
+    }
+}
+close $fh_sequences;
+
 
 if ($query =~ /^(.+)Species(\d+).fa/) {
     $filePath = $1;
@@ -23,4 +51,12 @@ else {
     die;
 }
 
-system("diamond blastp --ignore-warnings -d ${filePath}diamondDBSpecies${dataNumber}.dmnd -q ${filePath}Species${queryNumber}.fa -o Blast${queryNumber}_${dataNumber}.txt.gz -f 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen nident positive qframe qstrand gaps qseq --more-sensitive -p 1 --quiet -e 0.001 --compress 1");
+my $queryOrganism = $species_map{$queryNumber};
+my $dataOrganism = $species_map{$dataNumber};
+
+if (-e "/previousBlasts/$queryOrganism_$dataOrganism.txt.gz") {
+    system("cp /previousBlasts/$queryOrganism_$dataOrganism.txt.gz ./Blast$queryNumber_$databaseNumber.txt.gz");
+}
+else {
+    system("diamond blastp --ignore-warnings -d ${filePath}diamondDBSpecies${dataNumber}.dmnd -q ${filePath}Species${queryNumber}.fa -o Blast${queryNumber}_${dataNumber}.txt.gz -f 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen nident positive qframe qstrand gaps qseq --more-sensitive -p 1 --quiet -e 0.001 --compress 1");
+}
