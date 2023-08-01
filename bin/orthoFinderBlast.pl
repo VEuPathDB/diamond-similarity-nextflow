ad9ac12deb27bed56855e0b21d24536c1b4eb932
rdemko2332 <rdemko2332@gmail.com>
2023-08-01 10:45:23 -0400

    dataNumber variable

--- bin/orthoFinderBlast.pl
+++ bin/orthoFinderBlast.pl
@@ -55,7 +55,7 @@ my $queryOrganism = $species_map{$queryNumber};
 my $dataOrganism = $species_map{$dataNumber};
 
 if (-e "/previousBlasts/${queryOrganism}_${dataOrganism}.txt.gz") {
-    system("cp /previousBlasts/${queryOrganism}_${dataOrganism}.txt.gz ./Blast${queryNumber}_${databaseNumber}.txt.gz");
+    system("cp /previousBlasts/${queryOrganism}_${dataOrganism}.txt.gz ./Blast${queryNumber}_${dataNumber}.txt.gz");
 }
 else {
     system("diamond blastp --ignore-warnings -d ${filePath}diamondDBSpecies${dataNumber}.dmnd -q ${filePath}Species${queryNumber}.fa -o Blast${queryNumber}_${dataNumber}.txt.gz -f 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen nident positive qframe qstrand gaps qseq --more-sensitive -p 1 --quiet -e 0.001 --compress 1");
