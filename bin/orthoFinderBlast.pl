7e8718cc44eac5edacd962394b7cde8f9f49f423
rdemko2332 <rdemko2332@gmail.com>
2023-08-08 09:17:32 -0400

    Switching from sed to optimized regex

--- bin/orthoFinderBlast.pl
+++ bin/orthoFinderBlast.pl
@@ -6,9 +6,9 @@ use Getopt::Long;
 
 my ($query,$database,$filePath,$queryNumber,$dataNumber,$previousBlasts,$sequences_id_file,$species_id_file);
 
-&GetOptions("query=s" => \$query,
-            "database=s" => \$database,
-	    "sequences_id_file=s" => \$sequences_id_file,
+&GetOptions("query=s"=> \$query,
+            "database=s"=> \$database,
+	    "sequences_id_file=s"=> \$sequences_id_file,
 	    "species_id_file=s"=> \$species_id_file
            );
 
@@ -35,6 +35,18 @@ while (my $line = <$fh_species>) {
 }
 close $fh_species;
 
+# Read sequences ID file
+open my $fh_sequences, '<', $sequences_id_file or die "Cannot open $sequences_id_file: $!";
+my %real_to_new_seqs_map;
+while (my $line = <$fh_sequences>) {
+    chomp $line;
+    if ($line =~ /^(\d+_\d+):\s+(.+)/) {
+        my ($new, $real) = ($1, $2);
+        $real_to_new_seqs_map{$real} = $new;
+    }
+}
+close $fh_sequences;
+
 if ($query =~ /^(.+)Species(\d+).fa/) {
     $filePath = $1;
     $queryNumber = $2;
@@ -49,48 +61,34 @@ else {
     die;
 }
 
-# Read sequences ID file
-open my $fh_sequences, '<', $sequences_id_file or die "Cannot open $sequences_id_file: $!";
-my %real_to_new_seqs_map;
-while (my $line = <$fh_sequences>) {
-    chomp $line;
-    if ($line =~ /^(\d+_\d+):\s+(.+)/) {
-        my ($new, $real) = ($1, $2);
-	if ($new =~ /${queryNumber}_\d+/ || $new =~ /${dataNumber}_\d+/) {
-	    $real_to_new_seqs_map{$real} = $new;
-	}
-    }
-}
-close $fh_sequences;
-
 my $queryOrganism = $id_to_species_map{$queryNumber};
 my $dataOrganism = $id_to_species_map{$dataNumber};
-
-my $size = keys %real_to_new_seqs_map;
-print "Size is $size\n";
 
 if (-e "/previousBlasts/${queryOrganism}_${dataOrganism}.txt.gz") {
     system("cp /previousBlasts/${queryOrganism}_${dataOrganism}.txt.gz ./Blast${queryNumber}_${dataNumber}.txt.gz");
     system("gunzip Blast${queryNumber}_${dataNumber}.txt.gz");
     # Rename files in the output directory
-    my $sed_search_string = "";
-    my $sed_string_count = 0;
-    foreach my $key (keys %real_to_new_seqs_map) {
-	my $new_id = $real_to_new_seqs_map{$key};
-	$key =~ s/\//\\\//g;
-	$sed_search_string = $sed_search_string . "s/${key}/${new_id}/g;";
-	$sed_string_count += 1;
-	if ($sed_string_count % 1000 == 0) {
-	    system("sed -i \"${sed_search_string}\" Blast${queryNumber}_${dataNumber}.txt");
-	    $sed_string_count = 0;
-	    $sed_search_string = "";
+    open my $inputFile, '<', "Blast${queryNumber}_${dataNumber}.txt" or die "Cannot open \"Blast${queryNumber}_${dataNumber}.txt\": $!";
+    my $new_name_temp = "Blast${queryNumber}_${dataNumber}.txt" . ".temp";
+    open(OUT,">$new_name_temp");
+    while (my $line = <$inputFile>) {
+   	chomp $line;
+	if ($line =~ /^([^\t]*)\t([^\t]*)\t(.+)$/) {
+            my ($organism_first_sequence, $organism_second_sequence, $data) = ($1,$2,$3);
+	    my $new_first_sequence = $real_to_new_seqs_map{$organism_first_sequence};
+	    my $new_second_sequence = $real_to_new_seqs_map{$organism_second_sequence};
+	    print OUT "$new_first_sequence\t$new_second_sequence\t$data\n";
+        }
+	else {
+            die "$line Invalid blast format\n";
 	}
     }
-    #If happens to end on a multiple of 1000, this will unneededly be rerun
-    system("sed -i \"${sed_search_string}\" Blast${queryNumber}_${dataNumber}.txt");
+    system("mv $new_name_temp Blast${queryNumber}_${dataNumber}.txt");
     system("gzip Blast${queryNumber}_${dataNumber}.txt");
+    close OUT;
 }
 else {
+    print "Calculating New!\n";
     if ($query =~ /^(.+)Species(\d+).fa/) {
         $filePath = $1;
         $queryNumber = $2;
