36787748622546e830e1b1c83cc1ef306ca7ced2
rdemko2332 <rdemko2332@gmail.com>
2023-09-12 16:44:12 -0400

    Changing order of orthoFinder blast output

--- bin/orthoFinderBlast.pl
+++ bin/orthoFinderBlast.pl
@@ -102,5 +102,5 @@ else {
     else {
         die;
     }
-    system("diamond blastp --ignore-warnings -d ${filePath}diamondDBSpecies${dataNumber}.dmnd -q ${filePath}Species${queryNumber}.fa -o Blast${queryNumber}_${dataNumber}.txt.gz -f 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen nident positive qframe qstrand gaps qseq qcovhsp scovhsp --more-sensitive -p 1 --quiet -e 0.001 --compress 1");
+    system("diamond blastp --ignore-warnings -d ${filePath}diamondDBSpecies${dataNumber}.dmnd -q ${filePath}Species${queryNumber}.fa -o Blast${queryNumber}_${dataNumber}.txt.gz -f 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen nident positive qframe qstrand gaps qcovhsp scovhsp qseq --more-sensitive -p 1 --quiet -e 0.001 --compress 1");
 }
